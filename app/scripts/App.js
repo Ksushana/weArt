cbb70460c0a36524d9945f4b6f9bb5f2aa59a1b8
Ksushana <31410971+Ksushana@users.noreply.github.com>
2021-11-22 17:17:38 +0100

    js

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -7,17 +7,13 @@
 /*========================================
 // Utils
 ========================================*/
-import swiperjs from "./utils/swiper";
-import swipeEvent from "./utils/swipe-event";
 import responsive from "./utils/responsive";
 /*========================================
 // Modules
 ========================================*/
 
 import tabs from "./modules/tabs";
-import swiper from "./modules/swiper";
 import reset from "./modules/reset";
-import grid from "./modules/grid";
 import menu from "./modules/menu";
 import filter from "./modules/filter-mobile";
 import range from "./modules/range";
