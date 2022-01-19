850f5bc8d8e5403a702efb3a3d8ac6b93acd686a
Ksushana <31410971+Ksushana@users.noreply.github.com>
2022-01-19 16:26:43 +0100

    add new pages

--- app/scripts/modules/project-slider.js
+++ app/scripts/modules/project-slider.js
@@ -8,3 +8,23 @@ const swiper = new Swiper(".project-item__slider .swiper", {
   },
   slidesPerView:1
 });  
+if(window.innerWidth  > 767)
+new Swiper(".item__more-list", {
+  navigation: {
+    nextEl: ".item__more__button-next",
+    prevEl: ".item__more__button-prev"
+  },
+  spaceBetween: 24,
+  slidesPerView: "auto"
+});  
+
+if(window.innerWidth  > 767) {
+new Swiper(".projects__slider", {
+  navigation: {
+    nextEl: ".projects-slider__button-next",
+    prevEl: ".projects-slider__button-prev"
+  },
+  spaceBetween: 24,
+  slidesPerView: "auto"
+});  
+}
--- app/scripts/modules/tabs.js
+++ app/scripts/modules/tabs.js
@@ -33,4 +33,39 @@ window.addEventListener("load", function() {
 	}
 });
 
+window.addEventListener("load", function() {
+	const myTabsArtist = document.querySelectorAll(".js-artist-tab-link");
+	
+	if (myTabsArtist !== null && myTabsArtist.length > 0) {
+		function myTabClicks(tabClickEvent) {
+			for (var i = 0; i < myTabsArtist.length; i++) {
+				myTabsArtist[i].classList.remove("is-active");
+			}
+	
+			var clickedTab = tabClickEvent.currentTarget; 
+	
+			clickedTab.classList.add("is-active");
+	
+			tabClickEvent.preventDefault();
+	
+			var myContentPanes = document.querySelectorAll(".js-artist-tab-pane");
+	
+			for (i = 0; i < myContentPanes.length; i++) {
+				myContentPanes[i].classList.remove("is-active");
+			}
+	
+			var anchorReference = tabClickEvent.target;
+			var activePaneId = anchorReference.getAttribute("href");
+			var activePane = document.querySelector(activePaneId);
+	
+			activePane.classList.add("is-active");
+	
+		}
+	
+		for (i = 0; i < myTabsArtist.length; i++) {
+			myTabsArtist[i].addEventListener("click", myTabClicks)
+		}
+	}
+});
+
 
