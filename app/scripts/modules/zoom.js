6169bfb4ca46b9b5ad875788506074279ed0ef14
Ksushana <31410971+Ksushana@users.noreply.github.com>
2022-01-20 15:04:19 +0100

    fix zoom

--- app/scripts/modules/zoom.js
+++ app/scripts/modules/zoom.js
@@ -1,19 +1,20 @@
 const imageContainer = document.querySelector('.zoom');
-imageContainer.onmousemove = (event) => {zoom(event)};
+if (imageContainer) {
+  imageContainer.onmousemove = (event) => {zoom(event)};
+  const zoom = (e) => {
+    let imageZoom = e.currentTarget;
+    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
+    e.offsetY ? offsetY = e.offsetY : offsetY = e.touches[0].pageY;
+    x = offsetX/imageZoom.offsetWidth * 100;
+    y = offsetY/imageZoom.offsetHeight * 100;
+    imageZoom.style.backgroundPosition = x + '% ' + y + '%';
+  };
 
-const zoom = (e) => {
-  let imageZoom = e.currentTarget;
-  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
-  e.offsetY ? offsetY = e.offsetY : offsetY = e.touches[0].pageY;
-  x = offsetX/imageZoom.offsetWidth * 100;
-  y = offsetY/imageZoom.offsetHeight * 100;
-  imageZoom.style.backgroundPosition = x + '% ' + y + '%';
-};
-
-const thumbs = document.getElementsByClassName('thumb');
-for(var i = 0; i < thumbs.length; i++) {
-  thumbs[i].onclick = function() {
-    document.querySelector('.zoom img').src = this.src;
-    document.querySelector('.zoom').style.backgroundImage = 'url(' + this.src + ')';
-  }
-};
+  const thumbs = document.getElementsByClassName('thumb');
+  for(var i = 0; i < thumbs.length; i++) {
+    thumbs[i].onclick = function() {
+      document.querySelector('.zoom img').src = this.src;
+      document.querySelector('.zoom').style.backgroundImage = 'url(' + this.src + ')';
+    }
+  };
+}
