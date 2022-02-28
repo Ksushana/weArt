4d5d1f2804e5dc083656c74c1d6a6d730367c62d
Ksushana <31410971+Ksushana@users.noreply.github.com>
2022-02-28 13:48:44 +0100

    fix slider

--- app/scripts/modules/item-slider.js
+++ app/scripts/modules/item-slider.js
@@ -1,36 +1,40 @@
 import Swiper, { Navigation, Thumbs } from 'swiper';
 Swiper.use([Navigation, Thumbs]);
 
-const galleryThumbs = new Swiper(".item__img-slider-thumbs", {
-  spaceBetween: 12,
-  slidesPerView: 3,
-  freeMode: true,
-  centeredSlides: true,
-  watchSlidesProgress: true,
-  slideToClickedSlide: true,  
-  loop: true,
-  breakpoints: {
-    320: {
-      spaceBetween: 5
+const itemSlides = document.querySelectorAll('.item__img-slider .swiper-slide');
+const itemNavigation = document.querySelector('.item__img-slider-navigation');
+if (itemSlides.length > 1) {
+  itemNavigation.classList.add('is-shown');
+  const galleryThumbs = new Swiper(".item__img-slider-thumbs", {
+    spaceBetween: 12,
+    slidesPerView: 3,
+    freeMode: true,
+    centeredSlides: true,
+    watchSlidesProgress: true,
+    slideToClickedSlide: true,  
+    loop: true,
+    breakpoints: {
+      320: {
+        spaceBetween: 5
+      },
+      769: {
+        spaceBetween: 12
+      },
+    }
+  }); 
+  
+  const galleryTop = new Swiper(".item__img-slider", {
+    navigation: {
+      nextEl: ".item__img-slider-next",
+      prevEl: ".item__img-slider-prev"
     },
-    769: {
-      spaceBetween: 12
+    spaceBetween: 24,
+    slidesPerView: 1,
+    loop: true,
+    thumbs: {
+      swiper: galleryThumbs,
     },
-  }
-}); 
+  }); 
 
-const galleryTop = new Swiper(".item__img-slider", {
-  navigation: {
-    nextEl: ".item__img-slider-next",
-    prevEl: ".item__img-slider-prev"
-  },
-  spaceBetween: 24,
-  slidesPerView: 1,
-  loop: true,
-  thumbs: {
-    swiper: galleryThumbs,
-  },
-}); 
-
-console.log(galleryTop);
-console.log(galleryThumbs);
+  
+}
